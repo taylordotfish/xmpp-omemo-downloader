bdc9e96c9ca65db9c5066739ee960cbdc93a2425
taylor.fish <contact@taylor.fish>
2021-02-04 16:37:35 -0800

    Update license header

--- xmpp-omemo-downloader.py
+++ xmpp-omemo-downloader.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-# Requires Python packages `pycryptodome` and `requests`.
 # Copyright (C) 2020 taylor.fish <contact@taylor.fish>
-# License: GNU Affero General Public License, version 3 or later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 from Crypto.Cipher import AES
 from requests.exceptions import RequestException
